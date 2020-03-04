5e345601255e208fa0ce0c00560626c55f87cb89
Borfoord <borfoord@yahoo.com>
2020-03-04 16:36:55 +0200

    Rubric model

--- app/Rubric.php
+++ app/Rubric.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rubric extends Model
 {
-    //
+    protected $fillable = ['title', 'parent_id', 'created_by', 'modified_by'];
+
+    public function children() {
+                return $this->hasMany(self::class, 'parent_id');
+    }
 }
